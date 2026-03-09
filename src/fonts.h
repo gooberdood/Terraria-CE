ed861850c2aaeeb965847d9bd5ce411d3b3c5a33
gooberdev <rodimongavyn@gmail.com>
2026-03-08 23:27:38 -0400

    Delete src directory

--- src/fonts.h
+++
@@ -1,4 +0,0 @@
-extern const fontlib_font_t *andy12;
-extern const fontlib_font_t *andy14;
-extern const fontlib_font_t *andy16;
-extern const fontlib_font_t *andy18;
--- src/loadSprites.h
+++
@@ -1,8 +0,0 @@
-#include <compression.h>
-#include <graphx.h>
-#include "gfx/traspr.h"
-
-extern gfx_sprite_t *goober;
-
-
-void decompressSprites(void);
